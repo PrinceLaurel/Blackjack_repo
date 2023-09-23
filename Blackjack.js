e25576a04822a821b157f31f4769b5dc8d647a53
PrinceLaurel <prince.laurel@edu.huddinge.se>
2023-09-23 12:55:58 +0200

    skapade shuffle och dealcards function

--- Blackjack.js
+++ Blackjack.js
@@ -1,16 +1,24 @@
-window.onload = function() {
-    buildDeck();
-}
+let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let suits = ["C", "D", "H", "S"];
+let deck = [];
 
-function buildDeck () {
-    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-    let types = ["C", "D", "H", "S"];
-    deck = [];
-
-    for (let i = 0; i < types.length; i++) {
-        for (let j = 0; j < values.length; j++) {
-            deck.push(values[j] + "-" + types[i]);
-        }
+for (let suit of suits) {
+    for (let value of values) {
+        deck.push(`${suit}-${value}`);
     }
-    console.log(deck);
 }
+
+function shuffle(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+    return deck;
+}
+
+function dealCards(deck, numCardsPerHand) {
+    return deck.splice(0, numCardsPerHand);
+}
+
+let playerhand = dealCards(0, 2);
+let dealerhand = dealCards(0, 2);
